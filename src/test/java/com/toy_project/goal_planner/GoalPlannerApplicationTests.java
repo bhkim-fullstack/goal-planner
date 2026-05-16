422f20bc55a668b9060f8c6a610327d528cb2db7
BH <kbh02180@gamil.com>
2026-05-17 00:45:02 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/toy_project/goal_planner/GoalPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy_project.goal_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
