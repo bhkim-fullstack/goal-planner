be6d0af69118e69c47520b22d063d692efe70965
BH <kbh02180@gamil.com>
2026-05-18 10:59:12 +0900

    feat: 회원 엔티티 생성 및 레토지토리 생성

--- src/main/java/com/toy_project/goal_planner/domain/user/User.java
+++ src/main/java/com/toy_project/goal_planner/domain/user/User.java
@@ -1,11 +1,8 @@
 package com.toy_project.goal_planner.domain.user;
 
+import com.toy_project.goal_planner.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -13,26 +10,22 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class User {
+public class User extends BaseEntity {
 
+    // 사용자 고유 식별자
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 사용자 이메일 (로그인 ID)
     @Column(nullable = false, unique = true)
     private String email;
 
+    // 사용자 비밀번호 (암호화 저장)
     @Column(nullable = false)
     private String password;
 
+    // 사용자 닉네임
     @Column(nullable = false)
     private String nickname;
-
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    @Column(nullable = false)
-    private LocalDateTime updatedAt;
 }
